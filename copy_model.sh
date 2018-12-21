1233648fc59778685d560336478be9bb71268518
Tambet Matiisen <tambet.matiisen@gmail.com>
2018-12-21 14:58:42 +0200

    Minor changes to scripts.

--- copy_model.sh
+++ copy_model.sh
@@ -1,8 +1,9 @@
-folder=$1
-size=$2
+dir=$1
+folder=$2
+size=$3
 mkdir -p $folder/model$size
-scp -r "rocket.hpc.ut.ee:christmas2018/data/ati/$folder/model$size/checkpoint" $folder/model$size
-scp -r "rocket.hpc.ut.ee:christmas2018/data/ati/$folder/model$size/model*" $folder/model$size
+scp -r "rocket.hpc.ut.ee:christmas2018/data/$dir/$folder/model$size/checkpoint" $folder/model$size
+scp -r "rocket.hpc.ut.ee:christmas2018/data/$dir/$folder/model$size/model*" $folder/model$size
 
 #python reduce_model.py --model-input $folder/model$size --model-output $folder/model$size
 #python freeze_model.py --model-folder $folder/model$size
--- finalize_model.sh
+++ finalize_model.sh
@@ -1,5 +1,5 @@
 folder=$1
 size=$2
 
-python reduce_model.py --model-input $folder/model$size --model-output $folder/model$size
-python freeze_model.py --model-folder $folder/model$size
+python reduce_model.py --model-input $folder/model$size --model-output $folder/model${size}_reduced
+python freeze_model.py --model-folder $folder/model${size}_reduced
